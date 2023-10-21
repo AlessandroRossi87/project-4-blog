70e72e96780b67d5476320fe1d7625860be141ea
AlessandroRossi87 <>
2023-10-21 13:00:44 +0000

    Completed post detail template

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('category/<str:category_name>/',
-         views.category, name='category-detail'),
+         views.category, name='category_detail'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
