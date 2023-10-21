b7ddf0893d91a0f899f212ae601cde27f9c4e3c5
AlessandroRossi87 <>
2023-10-21 11:41:17 +0000

    Created category model and view

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,17 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+        
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -8,3 +8,13 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
+
+def category(request, category_name):
+    category = get_object_or_404(Category, id=category_name)
+    posts = Post.objects.filter(category=category, active=True)
+
+    return render(request, 'index.html', {
+        'category': category,
+        'posts': posts,
+    })
