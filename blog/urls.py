3afc92009cd31cd667183a5a540b43f7ad07b920
AlessandroRossi87 <>
2023-10-22 11:57:08 +0000

    Users can post new alert

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('new_post/', views.NewPost.as_view(), name='new_post'),
     path('', views.PostList.as_view(), name='home'),
     path('category/<str:category_name>/',
          views.category, name='category'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('newpost/', views.NewPost.as_view(), name='new_post'),
 ]
