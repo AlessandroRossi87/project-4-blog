bab2e772637d1d197054629100fcbb10cbdc4505
AlessandroRossi87 <>
2023-10-24 12:33:29 +0000

    Added timer script to js file

--- blog/forms.py
+++ blog/forms.py
@@ -17,39 +17,3 @@ class NewPostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'slug', 'featured_image',
                   'excerpt', 'content', 'category', 'status']
-
-# class NewPostForm(forms.ModelForm):
-#    class Meta:
-#        model = Post
-#        fields = ('category', 'title', 'content', 'featured_image',)
-#        widgets = {
-#            'category': forms.Select(attrs={
-#                'class': INPUT_CLASSES
-#            }),
-#            'title': forms.TextInput(attrs={
-#                'class': INPUT_CLASSES
-#            }),
-#            'content': forms.Textarea(attrs={
-#                'class': INPUT_CLASSES
-#            }),
-#            'featured_image': forms.FileInput(attrs={
-#                'class': INPUT_CLASSES
-#            })
-#        }
-
-
-# class EditPostForm(forms.ModelForm):
-#    class Meta:
-#        model = Post
-#        fields = ('title', 'content', 'featured_image',)
-#        widgets = {
-#            'title': forms.TextInput(attrs={
-#                'class': INPUT_CLASSES
-#            }),
-#            'content': forms.Textarea(attrs={
-#                'class': INPUT_CLASSES
-#            }),
-#            'featured_image': forms.FileInput(attrs={
-#                'class': INPUT_CLASSES
-#            })
-#        }
--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,10 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenu.addEventListener("click", () => {
-  document.querySelector(".menu").classList.toggle("show");
+    document.querySelector(".menu").classList.toggle("show");
 });
+
+setTimeout(function () {
+    let messages = document.getElementById("msg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 2000);
