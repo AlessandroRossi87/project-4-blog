2c0e563fa3ac3f08cc6243c1b9ee43f78a8954bc
AlessandroRossi87 <>
2023-10-27 11:49:31 +0000

    Moved post text in post detail template

--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,4 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenu.addEventListener("click", () => {
-    document.querySelector(".menu").classList.toggle("show");
+  document.querySelector(".menu").classList.toggle("show");
 });
