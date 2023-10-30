c8c57edaf24184313ffec373b771836d8928994f
AlessandroRossi87 <>
2023-10-30 11:01:47 +0000

    Created widgets for newpostform

--- blog/forms.py
+++ blog/forms.py
@@ -3,9 +3,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-# INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
-
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -15,4 +12,10 @@ class CommentForm(forms.ModelForm):
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'featured_image', 'content', 'category']
+        fields = ('title', 'featured_image', 'content', 'category')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'featured_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
